b43771b3dce722009335143172dfab759cb9114a
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2023-06-15 18:37:46 +0630

    fix : build req error

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: NextApiRequest) {
+export async function GET() {
   return NextResponse.json({ message: "Hello NextJs" });
 }
